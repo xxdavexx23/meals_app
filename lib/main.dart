37f31f9f509bec565e48e70c0ba82ef180c2aca0
David Isaac Cohen <dc@Davids-Air-433.attlocal.net>
2023-03-13 02:57:20 -0400

    Added OnGenerate Route

--- lib/main.dart
+++ lib/main.dart
@@ -1,6 +1,7 @@
 import 'package:flutter/material.dart';
 import 'package:meals_app/screens/categories_screen.dart';
 import 'package:meals_app/screens/category_meals_screen.dart';
+import 'package:meals_app/screens/meal_detail_screen.dart';
 
 void main() => runApp(MyApp());
 
@@ -31,6 +32,14 @@ class MyApp extends StatelessWidget {
         routes: {
           '/': (ctx) => CategoriesScreen(),
           CategoryMealsScreen.routeName: (ctx) => CategoryMealsScreen(),
+          //MealDetailScreen.routeName: (ctx) => MealDetailScreen(),
+        },
+        onGenerateRoute: (settings) {
+          print(settings.arguments);
+          return MaterialPageRoute(builder: (ctx) => CategoriesScreen());
+        },
+        onUnknownRoute: (settings) {
+          return MaterialPageRoute(builder: (ctx) => CategoriesScreen());
         });
   }
 }
--- lib/widgets/meal_item.dart
+++ lib/widgets/meal_item.dart
@@ -1,17 +1,21 @@
 //create a stateless widget called MealItem that returns a card with a column widget inside it
 
 import 'package:flutter/material.dart';
+import 'package:meals_app/screens/meal_detail_screen.dart';
 
 import '../models/meal.dart';
 
 class MealItem extends StatelessWidget {
+  final String id;
   final String title;
   final String imageUrl;
   final int duration;
   final Complexity complexity;
   final Affordability affordability;
 
-  void selectMeal() {}
+  void selectMeal(BuildContext context) {
+    Navigator.of(context).pushNamed(MealDetailScreen.routeName, arguments: id);
+  }
 
   String get complexityText {
     switch (complexity) {
@@ -40,7 +44,8 @@ class MealItem extends StatelessWidget {
   }
 
   MealItem(
-      {required this.title,
+      {required this.id,
+      required this.title,
       required this.imageUrl,
       required this.duration,
       required this.complexity,
@@ -49,7 +54,7 @@ class MealItem extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     return InkWell(
-        onTap: selectMeal,
+        onTap: () => selectMeal(context),
         child: Card(
           shape: RoundedRectangleBorder(
             borderRadius: BorderRadius.circular(15),
